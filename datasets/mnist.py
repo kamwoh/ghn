983b0dfbcacc9f6040165e2734e0b65ed34ae6ce
kamwoh <kamwoh@neruti.com>
2017-12-26 01:57:34 +0800

    removed datasets

--- datasets/mnist.py
+++
@@ -1,7 +0,0 @@
-from sklearn.datasets import fetch_mldata
-import os
-
-dirname = os.path.dirname(os.path.abspath(__file__))
-mnist = fetch_mldata('MNIST original', data_home='%s/%s' % (dirname, 'mnist'))
-
-mnist.data = mnist.data.reshape((-1, 28, 28))
