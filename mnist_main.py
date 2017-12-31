464f58af04e26c0a0f906ff0492c3188ca9fb767
kamwoh <kamwoh@neruti.com>
2017-12-31 16:56:24 +0800

    fixed shape problem

--- mnist_main.py
+++ mnist_main.py
@@ -10,12 +10,6 @@ def main():
     ##############
     (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
-    X_train = np.expand_dims(X_train, -1)
-    Y_train = np.expand_dims(Y_train, -1)
-
-    X_test = np.expand_dims(X_test, -1) / 255.
-    Y_test = np.expand_dims(Y_test, -1)
-
     split = int(len(X_train) * 0.2)
     indices = np.arange(len(X_train))
     np.random.seed(np.random.randint(1000000))
@@ -30,12 +24,14 @@ def main():
     X_train = X_train[split:] / 255.
     Y_train = Y_train[split:]
 
+    X_test = X_test / 255.
+
     #############
     # mnist net #
     #############
     net = MnistNetGHD(lr=0.1,
                       batch_size=32,
-                      input_shape=[28, 28, 1],
+                      input_shape=[32, 32, 3],
                       with_relu=True)
     net.train(1, X_train, Y_train, X_val, Y_val)
     net.evaluate(X_test, Y_test)
