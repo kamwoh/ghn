55c7248234dd0b4625743a08d9813d61ac7d6bdc
kamwoh <kamwoh@neruti.com>
2018-01-03 00:42:30 +0800

    can i try batch size of 512?

--- cifar_main.py
+++ cifar_main.py
@@ -43,7 +43,7 @@ def main():
     # mnist net #
     #############
     net = CifarNetGHD(lr=0.1,
-                      batch_size=128,
+                      batch_size=512,
                       input_shape=[32, 32, 3],
                       with_relu=True,
                       fuzziness_relu=True,
