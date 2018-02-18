8a31fe05306868fac6223abeaa25eaf884ff54dd
kamwoh <kamwoh@neruti.com>
2018-02-19 02:16:40 +0800

    checkpoint

--- realtime_training_visualisation.py
+++ realtime_training_visualisation.py
@@ -31,8 +31,7 @@ class MnistImageInput(object):
 
 
 class RealtimeModel(object):
-    def __init__(self, setting_window, init_learning_rate, init_batch_size, train_gen, val_gen):
-        self.setting_window = setting_window
+    def __init__(self, layer_choices, init_learning_rate, init_batch_size, train_gen, val_gen):
         self.learning_rate = init_learning_rate
         self.batch_size = init_batch_size
         self.train_gen = train_gen
@@ -42,6 +41,8 @@ class RealtimeModel(object):
         self.models_layers_dict = {}
         self.models_selected_layer_name = {}
         self.button_running = False
+        self.setting_window = SettingWindow(layer_choices, self.button_onclick, self.dropdown_callback,
+                                            self.entry_callback)
 
     def add_model(self, name, model, layers_dict):
         self.models[name] = model
@@ -145,6 +146,9 @@ def to_dict(layers):
     return ret
 
 
+def visualise_thread(realtime_model):
+    pass
+
 def main():
     batch_size = 32
     learning_rate = 0.1
@@ -170,7 +174,6 @@ def main():
 
     img_input = MnistImageInput()
     img_idx = 0
-    layer_idx = 0
 
     ghd_layers = [layer.name for layer in ghd_model.layers]
     ghd_layers_dict = to_dict(ghd_layers)
@@ -184,7 +187,10 @@ def main():
     layer_choices.add_choices('bn', bn_layers)
     layer_choices.add_choices('naive', naive_layers)
 
-    sw = SettingWindow(layer_choices, None, None, None)
+    realtime_model = RealtimeModel(layer_choices, learning_rate, batch_size, train_gen, val_gen)
+    realtime_model.add_model('ghd', ghd_model, ghd_layers_dict)
+    realtime_model.add_model('bn', bn_model, bn_layers_dict)
+    realtime_model.add_model('naive', naive_model, naive_layers_dict)
 
     is_changed = True
 
@@ -193,49 +199,9 @@ def main():
             is_changed = False
 
             img_disp, img = img_input.get_image(img_idx)
-
-            out = visutils.activation(img, ghd_model, layer_idx)
-            out_wo_ghd = visutils.activation(img, naive_model, layer_idx)
-
-            if len(out.shape) == 4:
-                is_conv = True
-                is_fc = False
-                out = np.transpose(out, (3, 1, 2, 0))
-                out_wo_ghd = np.transpose(out_wo_ghd, (3, 1, 2, 0))
-            else:
-                is_conv = False
-                is_fc = True
-                out = np.transpose(out, (1, 0))
-                out_wo_ghd = np.transpose(out_wo_ghd, (1, 0))
-
-            out = imageutils.normalize(out)
-            disp = imageutils.combine_and_fit(out, is_conv=is_conv, is_fc=is_fc, disp_w=800)
-
-            out_wo_ghd = imageutils.normalize(out_wo_ghd)
-            disp_wo_ghd = imageutils.combine_and_fit(out_wo_ghd, is_conv=is_conv, is_fc=is_fc, disp_w=800)
-
             cv2.imshow('input', img_disp)
-            cv2.imshow('disp ghd', disp)
-            cv2.imshow('disp_wo_ghd', disp_wo_ghd)
 
-            weight = ghd_model.get_weights()[0]
-            weight = imageutils.normalize_weights(weight, 'conv')
-            weight = np.transpose(weight, (3, 0, 1, 2))
-            weight_disp = imageutils.combine_and_fit(weight, is_weights=True, disp_w=400)
-
-            weight_wo_ghd = naive_model.get_weights()[0]
-            weight_wo_ghd = imageutils.normalize_weights(weight_wo_ghd, 'conv')
-            weight_wo_ghd = np.transpose(weight_wo_ghd, (3, 0, 1, 2))
-            weight_disp_wo_ghd = imageutils.combine_and_fit(weight_wo_ghd, is_weights=True, disp_w=400)
-
-            cv2.imshow('weight_disp_ghd', weight_disp)
-            cv2.imshow('weight_disp_wo_ghd', weight_disp_wo_ghd)
-
-            res = ghd_model.predict(img)[0]
-            print('output_ghd', np.argmax(res), res.max())
-
-            res = naive_model.predict(img)[0]
-            print('output_wo_ghd', np.argmax(res), res.max())
+            realtime_model.visualise(img)
 
         val = cv2.waitKey(1) & 0xFF
 
